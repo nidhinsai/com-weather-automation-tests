0385fed8cc4cfee4ecdff57d2866c1116eac782d
Nidhin Raj Panthal Padikkal <nidhinsaikripa@gmail.com>
2026-04-12 09:49:51 -0400

    fix: add explicit waits before city list interaction in WeatherHomePage

--- src/main/java/com/weather/pages/WeatherHomePage.java
+++ src/main/java/com/weather/pages/WeatherHomePage.java
@@ -5,13 +5,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 /**
- * This is a page class which contains different elements from the home page and also the methods related to them
+ * Page object for the Weather.com home page.
+ * Contains elements and methods for the location search functionality.
  */
 public class WeatherHomePage extends BasePage {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeatherHomePage.class);
 
     @FindBy(id = "LocationSearch_input")
     private WebElement locationSearchInput;
@@ -19,14 +24,8 @@ public class WeatherHomePage extends BasePage {
     @FindBy(id = "LocationSearch_listbox")
     private WebElement locationSearchListBox;
 
-    @FindBy(css = "a > span.styles--locationName--1R6PN")
-    private WebElement locationName;
-
-    @FindBy(css = "div > span[class*='LanguageSelector--unitDisplay']")
-    private WebElement languageSelectorUnitDisplay;
-
     /**
-     * Constructor for WeatherHomePage class
+     * Constructor for WeatherHomePage.
      *
      * @param driver the WebDriver instance
      */
@@ -35,22 +34,42 @@ public class WeatherHomePage extends BasePage {
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     * Inputs a location in the search box and selects the matching city from the dropdown.
+     *
+     * @param location the city name to search for
+     * @return a WeatherTodayPage after selecting the city
+     */
     public WeatherTodayPage inputLocation(String location) {
+        LOGGER.info("Searching for location: {}", location);
+        waitForElementToBeClickable(locationSearchInput);
         locationSearchInput.click();
         locationSearchInput.sendKeys(location);
+        waitForElementToBeVisible(locationSearchListBox);
         selectCity(location);
         return new WeatherTodayPage(driver);
     }
 
-    // Method to select the first city from the list that matches the provided string parameter
+    /**
+     * Selects the first city from the dropdown that matches the given city name.
+     *
+     * @param cityName the city name substring to match
+     */
     public void selectCity(String cityName) {
         List<WebElement> cityButtons = locationSearchListBox.findElements(By.tagName("button"));
+        if (cityButtons.isEmpty()) {
+            LOGGER.warn("No city suggestions found for: {}", cityName);
+            return;
+        }
         for (WebElement cityButton : cityButtons) {
             String buttonText = cityButton.getText();
             if (buttonText.contains(cityName)) {
+                LOGGER.info("Selecting city: {}", buttonText);
+                waitForElementToBeClickable(cityButton);
                 cityButton.click();
-                break;
+                return;
             }
         }
+        LOGGER.warn("No matching city button found for: {}", cityName);
     }
 }
