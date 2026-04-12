0f6c0740a226fd3b101cc0d32045a8d6d3c76e24
Nidhin Raj Panthal Padikkal <nidhinsaikripa@gmail.com>
2026-04-12 09:49:19 -0400

    feat: add headless ChromeOptions support to WebDriverProvider

--- src/main/java/com/weather/utils/WebDriverProvider.java
+++ src/main/java/com/weather/utils/WebDriverProvider.java
@@ -3,42 +3,65 @@ package com.weather.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 /**
- * Provides WebDriver instances based on the specified browser type using dependency injection.
+ * Provides WebDriver instances based on the specified browser type and headless flag.
  */
 public class WebDriverProvider {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebDriverProvider.class);
+
     private final WebDriver driver;
 
     /**
      * Initializes a WebDriver instance based on the specified browser type.
      *
-     * @param browser The type of browser (e.g., "chrome", "firefox").
+     * @param browser    The type of browser (e.g., "chrome", "firefox").
+     * @param headless   Whether to run in headless mode.
      */
-    public WebDriverProvider(String browser) {
-        this.driver = initializeDriver(browser);
+    public WebDriverProvider(String browser, boolean headless) {
+        LOGGER.info("Initializing {} driver (headless={})", browser, headless);
+        this.driver = initializeDriver(browser, headless);
         configureDriver(this.driver);
     }
 
-    private WebDriver initializeDriver(String browser) {
+    private WebDriver initializeDriver(String browser, boolean headless) {
         switch (browser.toLowerCase()) {
             case "chrome" -> {
                 WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                if (headless) {
+                    options.addArguments("--headless=new");
+                    options.addArguments("--no-sandbox");
+                    options.addArguments("--disable-dev-shm-usage");
+                    options.addArguments("--disable-gpu");
+                    options.addArguments("--window-size=1920,1080");
+                }
+                options.addArguments("--disable-extensions");
+                options.addArguments("--remote-allow-origins=*");
+                return new ChromeDriver(options);
             }
             case "firefox" -> {
                 WebDriverManager.firefoxdriver().setup();
-                return new FirefoxDriver();
+                FirefoxOptions options = new FirefoxOptions();
+                if (headless) {
+                    options.addArguments("--headless");
+                }
+                return new FirefoxDriver(options);
             }
             case "safari" -> {
                 WebDriverManager.safaridriver().setup();
                 return new SafariDriver();
             }
-            default -> throw new IllegalArgumentException("Invalid browser specified: " + browser);
+            default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
     }
 
@@ -61,6 +84,7 @@ public class WebDriverProvider {
      */
     public void quitDriver() {
         if (driver != null) {
+            LOGGER.info("Quitting driver");
             driver.quit();
         }
     }
